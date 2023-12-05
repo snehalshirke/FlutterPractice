38e1716e9508952d4b48123a7d9d67cc09e293cb
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-05 13:30:40 +0530

    retrofit api integration

--- Retrofitapicall/lib/api_service.dart
+++ Retrofitapicall/lib/api_service.dart
@@ -1,15 +1,16 @@
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart';
-import 'package:retrofitapicall/album_model.dart';
+import 'package:retrofitapicall/main.dart';
 part 'api_service.g.dart';
 
 class Apis {
-  static const String album = '/albums/1';
+  //static const String album = '/albums/1';
+  static const String album = '/posts';
 }
 
 @RestApi(baseUrl: "https://jsonplaceholder.typecode.com")
 abstract class ApiClient {
-  //factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient();
+  factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
 
   @GET(Apis.album)
   Future<Album> getAlbum();
--- Retrofitapicall/lib/main.dart
+++ Retrofitapicall/lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:retrofitapicall/api_service.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+Future<Album> fetchAlbum() async {
+  final client = ApiClient(
+    Dio(
+      BaseOptions(contentType: "application/json"),
+    ),
+  );
+
+  try {
+    final response = await client.getAlbum();
+    return response;
+  } catch (e) {
+    throw Exception("Failed to load Api");
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -28,7 +43,30 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              fetchAlbum().then((value) {
+                print(value.id);
+                print(value.title);
+                print(value.userId);
+              });
+            },
+            child: const Text('API Request'),
+          ),
+        ),
       ),
     );
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+  const Album({required this.id, required this.userId, required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(id: json['id'], userId: json['userId'], title: json['title']);
   }
 }
