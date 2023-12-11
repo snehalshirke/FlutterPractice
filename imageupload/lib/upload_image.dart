7df7fe151ca854be2f53ad039a30ca5e619e1319
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-11 12:42:07 +0530

    chnages to remove

--- imageupload/lib/upload_image.dart
+++ imageupload/lib/upload_image.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadImageScreen extends StatefulWidget {
   const UploadImageScreen({super.key});
@@ -8,16 +11,48 @@ class UploadImageScreen extends StatefulWidget {
 }
 
 class _UploadImageScreenState extends State<UploadImageScreen> {
+  File? image;
+  final _picker = ImagePicker();
+  bool showSpinner = false;
+  Future getImage() async {
+    final pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+      setState(() {});
+    } else {
+      print("No image selected");
+    }
+  }
+
+  Future<void> uploadImage() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Upload Image'),
       ),
-      body: const Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+        children: [
+          Container(
+              child: image == null
+                  ? const Center(
+                      child: Text('Pick Image'),
+                    )
+                  : Container(
+                      child: Center(
+                        child: Image.file(
+                          File(image!.path).absolute,
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ))
+        ],
       ),
     );
   }
