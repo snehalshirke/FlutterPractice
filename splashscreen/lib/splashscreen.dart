c4ba652da1a7fa768fb5040883071bb1656599c6
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-12 17:05:10 +0530

    firestore dependency added

---
+++ splashscreen/lib/splashscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashWidget extends StatefulWidget {
+  const SplashWidget({super.key});
+
+  @override
+  State<SplashWidget> createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends State<SplashWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Splash Screen'),
+    );
+  }
+}
