ec47caa1312484b670935ba2f606722a0eb30ccd
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-12 17:25:26 +0530

    changes

--- splashscreen/lib/splashscreen.dart
+++ splashscreen/lib/splashscreen.dart
@@ -9,9 +9,42 @@ class SplashWidget extends StatefulWidget {
 
 class _SplashWidgetState extends State<SplashWidget> {
   @override
+  void initState() {
+    super.initState();
+    // Future.delayed(Duration(seconds: 3){
+    //   Navigator.push(
+    //     context,
+    //      MaterialPageRoute(
+    //       builder: (context) => Onboarding(),
+    //       ),
+    //       );
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Splash Screen'),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.blueAccent,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/png/img logo.png",
+            ),
+            const Text(
+              "Medtech",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.white),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
