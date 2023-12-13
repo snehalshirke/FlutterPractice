e946056cbc5e5855114aa69d038cf3a4f9b123f9
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-13 15:35:54 +0530

    single screen added

---
+++ imgdownloaderusingurl/lib/single_download.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SingleDownloadScreen extends StatefulWidget {
+  const SingleDownloadScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SingleDownloadScreen> createState() => _SingleDownloadScreenState();
+}
+
+class _SingleDownloadScreenState extends State<SingleDownloadScreen> {
+  TextEditingController url = TextEditingController();
+  double? _progress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Download Any File From URL\nin Flutter',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: url,
+              decoration: const InputDecoration(label: Text('Url*')),
+            ),
+            const SizedBox(height: 16),
+            _progress != null
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () {}, child: const Text('Download')),
+          ],
+        ),
+      ),
+    );
+  }
+}
