424c478e5408b9a965fb771207b69b1628db4811
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-15 11:48:15 +0530

    refactor file name

--- mvvm/lib/view/login_view.dart
+++ mvvm/lib/view/login_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm/utils/utils.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<LoginView> createState() => _LoginViewState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
