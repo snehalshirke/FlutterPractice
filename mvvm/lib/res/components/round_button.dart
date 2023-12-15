ad23c30d90fec9d0b1ebb3c7ea7128cf1dbbc199
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-15 13:15:46 +0530

    common round btn file created

--- mvvm/lib/res/components/round_button.dart
+++ mvvm/lib/res/components/round_button.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class RoundButton extends StatelessWidget {
+  const RoundButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 200,
+      child: const Text('Login'),
+    );
+  }
+}
