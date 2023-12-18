cfd81cea30d9059582b0a03084b517791b7e4b42
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-18 17:01:01 +0530

    basic splash screen

---
+++ splashscreen/lib/splashscreen.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const SecondScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: FlutterLogo(size: MediaQuery.of(context).size.height));
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("GeeksForGeeks")),
+      body: const Center(
+          child: Text(
+        "Home page",
+        // ignore: deprecated_member_use
+        textScaleFactor: 2,
+      )),
+    );
+  }
+}
