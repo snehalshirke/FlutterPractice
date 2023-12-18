1976dc83e12b6fc5ef68169cbb9c9d9e2e58a292
SnehalSodel <snehal.s@sodelsolutions.com>
2023-12-18 13:26:34 +0530

    sharedpreference setup

--- mvvm/lib/main.dart
+++ mvvm/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mvvm/utils/routes/routes.dart';
 import 'package:mvvm/utils/routes/routes_name.dart';
 import 'package:mvvm/view_model/auth_view_model.dart';
+import 'package:mvvm/view_model/user_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -22,6 +23,9 @@ class _MyAppState extends State<MyApp> {
       providers: [
         ChangeNotifierProvider(
           create: (_) => AuthViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UserViewModel(),
         ),
       ],
       child: const MaterialApp(
