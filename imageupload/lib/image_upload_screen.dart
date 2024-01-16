e7eb2cd65452052b5cb2d629673638f5f6c2de6a
SnehalSodel <snehal.s@sodelsolutions.com>
2024-01-16 12:14:21 +0530

    imageupload changes

--- imageupload/lib/image_upload_screen.dart
+++ imageupload/lib/image_upload_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -11,44 +13,96 @@ class ImageUploadRoute extends StatefulWidget {
 class _ImageUploadState extends State<ImageUploadRoute> {
   XFile? image;
   final ImagePicker picker = ImagePicker();
+  Future getImage(ImageSource media) async {
+    var img = await picker.pickImage(source: media);
+
+    setState(() {
+      image = img;
+    });
+  }
+
+  void myAlert() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            title: const Text('Please choose media to select'),
+            content: SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      getImage(ImageSource.gallery);
+                    },
+                    child: const Row(
+                      children: [
+                        Icon(Icons.image),
+                        Text('From Gallery'),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      getImage(ImageSource.camera);
+                    },
+                    child: const Row(
+                      children: [
+                        Icon(Icons.camera),
+                        Text('From Camera'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: false,
-        backgroundColor: Colors.amber.shade100,
-        title: const Text(
-          'Image Slider',
-          style: TextStyle(fontSize: 14),
+        title: const Text('Upload Image'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                myAlert();
+              },
+              child: const Text('Upload Photo'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            image != null
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.file(
+                        File(image!.path),
+                        fit: BoxFit.cover,
+                        width: MediaQuery.of(context).size.width,
+                        height: 300,
+                      ),
+                    ),
+                  )
+                : const Text(
+                    "No Image",
+                    style: TextStyle(fontSize: 20),
+                  )
+          ],
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(18.0, 12.0, 16.0, 15.0),
-                child: Column(
-                  children: [
-                    _passportupload(),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          _bottomButton(),
-        ],
-      ),
     );
-  }
-
-  Widget _passportupload() {
-    return const Column();
-  }
-
-  Widget _bottomButton() {
-    return const Column();
   }
 }
